455036a5ffdfba88f4c63022de4659dca94166e7
Simon Hoke <simonhoke@gmail.com>
2024-05-17 11:45:00 -0700

    Simplify design

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   const meta = {
-    title: "Next.js Blog Starter Kit",
-    description: "Clone and deploy your own Next.js portfolio in minutes.",
-    image: "https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png",
+    title: "Hoke",
+    description: "Simon & Carter Hoke",
+    image: "https://hoke.dev/images/Simon.jpg",
   };
 
   return (
